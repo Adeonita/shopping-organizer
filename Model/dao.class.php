5bb28f05af13d35af7846e36eb10a9120fd2ee1b
adeonita <adeonita.sousa@gmail.com>
2020-04-25 14:49:59 -0300

    dao

--- Model/dao.class.php
+++ Model/dao.class.php
@@ -1,46 +1,13 @@
 <?php
-require_once('product.class.php');
-require_once('user.class.php');
-require_once('test.php');
 
 class Dao{
 
-    public function insert($object){  //Recebe o objeto e as propriedades a serem excedidas na montagem da query
-        $exceptions = $object->getExceptions();
-        $reflectionClass = new ReflectionClass($object); //Isntancia da reflection class tendo o objeto como parâmetro
-        $className = $reflectionClass->getName();  //pega a classe do objeto
-        $tableName = $className.'s';   //Coloca o nome da classe no plural
-        $classAritributes = $reflectionClass->getProperties();  //Pega todos os atributos da classe
-        $getNameAttributes = (array_column($classAritributes, 'name')); //pega apenas os nomes dos atributos
-        $properties = array_diff($getNameAttributes, $exceptions);  //Remoção do atributo indesejado 
-        $atributes = ""; 
-        $values = "";
-        foreach($properties as $property){  //Percorre apenas colunas com índice == name
-            $atributes = $atributes.$property.", ";  //e guarda esses atributos na variavel atributes, separados por virgula
-            $newName = ucfirst($property);   //Deixa a primeira letra de name(atributos) maiúscula
-            $values = $values.'"'.$object->{"get".$newName}().'"'. ", ";  //crio uma string que a cada loop corresponde a um método get da classe. ex object->getName(), pegando assim o nome, entre aspas e separados por virgula
-        }
+    /**
+     * @param $query
+     * Recebe a query pronta do query builder e faz de fato a inserção no banco de dados
+     */
+    public function insert($query){
 
-        $atributes = substr($atributes, 0, -2);  //Removo os dois últimos caracteres da string atributes, a saber " " e ',' respectivamente
-        $values = substr($values, 0, -2);        //Removo os dois últimos caracteres da string atributes, a saber " " e ',' respectivamente
-
-        if(empty($atributes)){
-            return;
-        }     
-        $query =  "INSERT INTO {$tableName} ({$atributes}) VALUES ({$values})";  
-       
-        return $query;
     }
-
-    
-
 }
-
-$dao = new Dao();
-$query = $dao->insert(new User('Adeonita','adeonita.sousa@gmail.com', "123testando"));
-$pd = $dao->insert(new Product("Sabonete", "Dove", "Higiene", 2,1));
-$mt = $dao->insert(new Test("teste"));
-
-print $query;
-print "<br>".$pd;
-print "<br>".$mt;
+?>
